4d2d2b3b9ab8a7f4bdc361a231d3a0538c654b42
Mahnoor kibrea <160492529+mahnoor-k29@users.noreply.github.com>
2024-05-07 14:17:17 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+ let myBalance =5000;
+ let myPin = 1234 ;
+ console.log("Welcome to my atm website");
+
+ let pinAnswer = await inquirer.prompt([
+{   name : "pin",
+    type : "number",
+    message:"Enter your pin code:"
+}])
+ if (pinAnswer.pin === myPin){
+    console.log("Pin is Correct, Login Successfully");
+    // console.log(`Current account balence is ${myBalance}`);
+    let operationAns = await inquirer.prompt([
+        {
+            name:"operation",
+            type:"list",
+            message:"Select an operation:",
+            choices:["Withdraw Amount","Check Balance"]
+        }])
+    if (operationAns.operation === "Withdraw Amount"){
+let amountAns= await inquirer.prompt([
+    {name:"amount",
+    type:"number",
+    message:"Enter the amount you withdraw:"}
+])
+if(amountAns.amount > myBalance){
+console.log("Insufficient Balance");
+}
+else{
+    myBalance -= amountAns.amount;
+    console.log(`${amountAns.amount} Withdraw successfully`);
+    console.log(`Your Remaining Balance is ${myBalance}`);
+}}
+    else if (operationAns.operation === "Check Balance"){
+console.log(`Your Account Balance is${myBalance}`);}
+ }
+ else{
+    console.log("Pin is Incorrect,Try Again");}
